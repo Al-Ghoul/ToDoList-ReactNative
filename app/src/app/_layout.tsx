d139e958cfcbd70e84953b649d1f6d311c284afc
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-02-27 05:16:03 +0200

    feat: move files to src/

--- app/src/app/_layout.tsx
+++ app/src/app/_layout.tsx
@@ -9,7 +9,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
+import { useColorScheme } from "@/src/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -17,7 +17,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
---
+++ app/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/src/components/ThemedText";
+import { View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <ThemedText>Hello</ThemedText>
+    </View>
+  );
+}
