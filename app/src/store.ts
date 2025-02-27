929ec1ee457ccac4100ef31b57e85028b6048857
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-02-27 07:52:31 +0200

    feat: setup redux provider & persist gate

---
+++ app/src/store.ts
@@ -0,0 +1,32 @@
+import {
+  ThunkAction,
+  Action,
+  combineReducers,
+  createStore,
+} from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({});
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
