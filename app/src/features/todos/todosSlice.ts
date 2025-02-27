dae5f876af4509a89402ac20640d890c61008069
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-02-27 07:53:18 +0200

    feat: add todos slice w/ reducers

---
+++ app/src/features/todos/todosSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "@/src/store";
+
+interface TodosState {
+  value: Todo[];
+}
+
+const initialState: TodosState = {
+  value: [],
+};
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.value.push({
+        //  NOTE: in a real app this should be a server generated id or at least not a numeric value
+        id: state.value.length + 1,
+        title: action.payload,
+        completed: false,
+      });
+    },
+    removeTodo: (state, action: PayloadAction<number>) => {
+      state.value = state.value.filter((todo) => todo.id !== action.payload);
+    },
+    completeTodo: (state, action: PayloadAction<number>) => {
+      state.value = state.value.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+    },
+  },
+});
+
+export const { addTodo, removeTodo, completeTodo } = todosSlice.actions;
+
+export const selectTodos = (state: RootState) => state.todos.value;
+
+export default todosSlice.reducer;
